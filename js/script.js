68cc723f225b2fad802bf30d104aa5149a85de98
Brian Bauska <brianbauska@gmail.com>
2026-05-20 18:56:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2430,19 +2430,19 @@ $.cssTransitionEnd = getTransitionEnd;
 $.findPos = findPos;
 })(jQuery);
 $(window).ready(function() {
-	  $('.pages').turn({
-      duration: 1500,
-      width: 620,
-      height: 445,
-	  //  acceleration: true,
+  $('.pages').turn({
+    duration: 1500,
+    width: 620,
+    height: 445,
+	//  acceleration: true,
     //  display: 'single',
-     // autoCenter: true,
-     turnCorners: "bl,br",
-	    elevation: 300,
-	    when: {
-	      turned: function(e, page) {
-	        console.log('Current view: ', $(this).turn('view'));
-	      }
+    // autoCenter: true,
+    turnCorners: "bl,br",
+	  elevation: 300,
+	  when: {
+	    turned: function(e, page) {
+	      console.log('Current view: ', $(this).turn('view'));
 	    }
-	  });
-	});
+      }
+  });
+});
